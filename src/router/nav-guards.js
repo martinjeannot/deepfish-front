6e2987d6465174d86b1ca3cfd9c57a73ff6063d7
Martin Jeannot <m4rtinjeannot@gmail.com>
2018-03-18 23:16:23 +0100

    [Auth] fix sign in

--- src/router/nav-guards.js
+++ src/router/nav-guards.js
@@ -2,30 +2,28 @@ import store from '../store';
 
 export default {
   authenticationGuard: (to, from, next) => {
-    if (to.matched.some(record => record.meta.authRequired)) {
-      if (store.getters.user) {
-        next();
-      } else {
-        const authToken = localStorage.getItem('auth_token');
-        if (authToken) {
-          store.dispatch('autoSignIn', JSON.parse(authToken))
-            .then(() => {
-              if (!store.getters.error) {
-                next();
-              } else {
-                next('/');
-              }
-            })
-            .catch(() => {
-              store.dispatch('setErrorAfterApiConsumption');
-              next('/');
-            });
-        } else { // no token found
-          next('/');
-        }
-      }
-    } else { // auth not required
+    if (store.getters.user) {
       next();
+    } else {
+      const authToken = localStorage.getItem('auth_token');
+      if (authToken) {
+        store.dispatch('autoSignIn', JSON.parse(authToken))
+          .then(() => {
+            if (!store.getters.error) {
+              next();
+            } else {
+              next('/');
+            }
+          })
+          .catch(() => {
+            store.dispatch('setErrorAfterApiConsumption');
+            next('/');
+          });
+      } else if (to.matched.some(record => record.meta.authRequired)) {
+        next('/');
+      } else { // auth not required
+        next();
+      }
     }
   },
   alertComponentGuard: (to, from, next) => {
