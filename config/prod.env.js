856db33859b4d717cfdc1256b2fef9013ce4245e
Martin Jeannot <m4rtinjeannot@gmail.com>
2018-05-16 16:29:26 +0200

    [Config] fix

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  DEEPFISH_BACK_BASE_URL: 'location.protocol + "//staging-api.deepfish.co"',
-  LINKEDIN_CLIENT_ID: '"77w79kdr6gql2h"',
+  DEEPFISH_BACK_BASE_URL: 'location.protocol + "//api.deepfish.co"',
+  LINKEDIN_CLIENT_ID: '"86rw9g7ie1fvw5"',
 };
