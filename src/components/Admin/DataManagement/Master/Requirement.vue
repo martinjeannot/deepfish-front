8ffb65e48f90da9067b0b8bda4927ae2e29826c1
Martin Jeannot <m4rtinjeannot@gmail.com>
2018-09-25 12:46:01 +0200

    [Admin/Requirement] refacto requirement display

--- src/components/Admin/DataManagement/Master/Requirement.vue
+++ src/components/Admin/DataManagement/Master/Requirement.vue
@@ -12,8 +12,7 @@
       <v-layout v-else>
         <v-flex xs12>
           <v-flex xs12 v-if="alertComponent">
-            <base-alert :type="alertComponent.type" :message="alertComponent.message"
-                        @dismissed="onAlertComponentDismissed"></base-alert>
+            <base-alert :type="alertComponent.type" :message="alertComponent.message" @dismissed="onAlertComponentDismissed"></base-alert>
           </v-flex>
           <v-layout v-if="requirement">
             <v-flex xs4 class="pr-3">
@@ -22,7 +21,13 @@
                   <v-flex xs12 class="text-xs-center mb-3">
                     <h2>{{ requirement.name }}</h2>
                   </v-flex>
-                  <v-flex xs12 class="mb-3">
+                  <v-flex v-if="requirement.typeform" xs12>
+                    <v-flex xs12 v-for="(answer, index) in typeformAnswers" :key="index" class="mt-2">
+                      <div class="font-weight-bold">{{ answer.question }}</div>
+                      <div>{{ answer.value }}</div>
+                    </v-flex>
+                  </v-flex>
+                  <v-flex v-else xs12 class="mb-3">
                     Since the
                     <span style="font-weight: bold">{{ requirement.createdAt | formatDate('LLL') }}</span>
                     <router-link :to="{ name: 'AdminDMCompany', params: {id: requirement.company.id} }">
@@ -47,8 +52,7 @@
               <v-card>
                 <v-card-text>
                   <v-text-field v-model="requirement.name" label="Requirement name"></v-text-field>
-                  <v-textarea label="Notes" v-model="requirement.notes" prepend-inner-icon="lock"
-                              rows="27"></v-textarea>
+                  <v-textarea label="Notes" v-model="requirement.notes" prepend-inner-icon="lock" rows="27"></v-textarea>
                   <div class="text-xs-right">
                     <v-btn icon fab small color="primary" @click="saveRequirement(requirement)">
                       <v-icon>done</v-icon>
@@ -65,77 +69,86 @@
 </template>
 
 <script>
-  import { mapGetters, mapActions } from 'vuex';
-  import DataManagementNavigation from '../Navigation';
+import { mapGetters, mapActions } from 'vuex';
+import DataManagementNavigation from '../Navigation';
 
-  export default {
-    name: 'data-management-requirement',
-    components: { DataManagementNavigation },
-    props: ['id'],
-    data: () => ({
-      requirement: null,
-      opportunitiesCounts: null,
-    }),
-    computed: {
-      ...mapGetters([
-        'api',
-        'initialLoading',
-        'loading',
-        'alertComponent',
-      ]),
-      doughnutChartData() {
-        return {
-          labels: ['Accepted', 'Pending', 'Declined'],
-          datasets: [
-            {
-              backgroundColor: ['#25CD73', '#FFC62E', '#FF5D2E'],
-              data: [this.opportunitiesCounts.acceptedCount, this.opportunitiesCounts.pendingCount,
-                this.opportunitiesCounts.declinedCount],
-            },
-          ],
-        };
-      },
+export default {
+  name: 'data-management-requirement',
+  components: { DataManagementNavigation },
+  props: ['id'],
+  data: () => ({
+    requirement: null,
+    opportunitiesCounts: null,
+  }),
+  computed: {
+    ...mapGetters(['api', 'initialLoading', 'loading', 'alertComponent']),
+    typeformAnswers() {
+      return this.requirement.typeform.form_response.answers.map((answer) => {
+        const question = this.requirement.typeform.form_response.definition.fields.find(
+          field => field.id === answer.field.id,
+        ).title;
+        let value = null;
+        switch (answer.type) {
+          case 'text':
+            value = answer.text;
+            break;
+          default:
+            value = 'N/A';
+        }
+        return { question, value };
+      });
     },
-    methods: {
-      ...mapActions([
-        'prepareForApiConsumption',
-        'clearLoading',
-        'showSnackbar',
-        'showSuccessSnackbar',
-        'setErrorAfterApiConsumption',
-        'onAlertComponentDismissed',
-      ]),
-      saveRequirement(requirement) {
-        const requirementData = Object.assign({}, requirement);
-        delete requirementData.company;
-        delete requirementData.jobType;
-        delete requirementData.seniority;
-        this.api
-          .patch(requirement._links.self.href, requirementData)
-          .then(() => this.showSnackbar('Success'))
-          .catch(() => this.showSnackbar('Error'));
-      },
+    doughnutChartData() {
+      return {
+        labels: ['Accepted', 'Pending', 'Declined'],
+        datasets: [
+          {
+            backgroundColor: ['#25CD73', '#FFC62E', '#FF5D2E'],
+            data: [
+              this.opportunitiesCounts.acceptedCount,
+              this.opportunitiesCounts.pendingCount,
+              this.opportunitiesCounts.declinedCount,
+            ],
+          },
+        ],
+      };
     },
-    created() {
-      this.prepareForApiConsumption(true);
-      Promise
-        .all([
-          this.api(`/requirements/${this.id}?projection=admin`),
-          this.api(`/opportunities/counts?requirementId=${this.id}`),
-        ])
-        .then(([
-                 requirementResponse,
-                 opportunitiesCountsResponse,
-               ]) => {
-          this.requirement = requirementResponse.data;
-          this.opportunitiesCounts = opportunitiesCountsResponse.data;
-        })
-        .catch(() => this.setErrorAfterApiConsumption())
-        .finally(() => this.clearLoading(true));
+  },
+  methods: {
+    ...mapActions([
+      'prepareForApiConsumption',
+      'clearLoading',
+      'showSnackbar',
+      'showSuccessSnackbar',
+      'setErrorAfterApiConsumption',
+      'onAlertComponentDismissed',
+    ]),
+    saveRequirement(requirement) {
+      const requirementData = Object.assign({}, requirement);
+      delete requirementData.company;
+      delete requirementData.jobType;
+      delete requirementData.seniority;
+      this.api
+        .patch(requirement._links.self.href, requirementData)
+        .then(() => this.showSnackbar('Success'))
+        .catch(() => this.showSnackbar('Error'));
     },
-  };
+  },
+  created() {
+    this.prepareForApiConsumption(true);
+    Promise.all([
+      this.api(`/requirements/${this.id}?projection=admin`),
+      this.api(`/opportunities/counts?requirementId=${this.id}`),
+    ])
+      .then(([requirementResponse, opportunitiesCountsResponse]) => {
+        this.requirement = requirementResponse.data;
+        this.opportunitiesCounts = opportunitiesCountsResponse.data;
+      })
+      .catch(() => this.setErrorAfterApiConsumption())
+      .finally(() => this.clearLoading(true));
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
