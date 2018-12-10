011e5a084685e5f1f2474da1dc8e25cc7cde3bcb
Martin Jeannot <m4rtinjeannot@gmail.com>
2018-12-10 18:26:00 +0100

    [Employer/Requirements] modularize calendly url

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,4 +4,5 @@ module.exports = {
   DEEPFISH_BACK_BASE_URL: 'location.protocol + "//staging-api.deepfish.co"',
   LINKEDIN_CLIENT_ID: '"77w79kdr6gql2h"',
   REQUIREMENT_CREATION_TYPEFORM_URL: '"https://deepfish.typeform.com/to/zO6LFY?employer="',
+  REQUIREMENT_CALENDLY_URL: '"https://calendly.com/deepfish/_staging_requirement"',
 };
