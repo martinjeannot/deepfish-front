1d7763825f5f0e4af2cb08116945c07c92ffc097
Martin Jeannot <m4rtinjeannot@gmail.com>
2019-01-25 19:01:37 +0100

    [Itw] wording

--- src/components/Talent/Opportunities/OpportunityInterviews.vue
+++ src/components/Talent/Opportunities/OpportunityInterviews.vue
@@ -89,7 +89,7 @@
         </v-card-title>
         <v-card-text>
           <v-flex xs12 class="subheading grey--text text--darken-2">
-            Acceptes-tu cet entretien avec {{ opportunity.company.name }}
+            Confirmes-tu cet entretien avec {{ opportunity.company.name }}
             le {{ selectedInterview.startAt | formatDate('dddd') }} {{ selectedInterview.startAt | formatDate('LL') }}
             à {{ selectedInterview.startAt | formatDate('LT') }} ?
           </v-flex>
