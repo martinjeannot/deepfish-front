588f102e14b5aafbc2980cd3abe377047600e207
Martin Jeannot <m4rtinjeannot@gmail.com>
2019-06-13 21:39:36 +0200

    [Admin/Interviews] allow admins to search by talent firstname and lastname + add search box component

--- src/components/Admin/DataManagement/Master/Interviews.vue
+++ src/components/Admin/DataManagement/Master/Interviews.vue
@@ -4,51 +4,62 @@
       <data-management-navigation></data-management-navigation>
     </v-flex>
     <v-flex xs10>
-      <v-flex xs12 class="pb-3">
-        <v-card>
-          <requirement-select
-            v-model="requirement"
-            @input="getInterviews"
-            class="pa-3"
-          ></requirement-select>
-        </v-card>
-      </v-flex>
-      <v-flex xs12>
-        <v-card>
-          <v-data-table
-            :headers="headers"
-            :items="interviews"
-            :loading="loading"
-            :pagination.sync="pagination"
-            :total-items="totalItems"
-          >
-            <template slot="items" slot-scope="props">
-              <td>{{ props.item.createdAt | formatDate('LLL') }}</td>
-              <td>
-                <router-link :to="{ name: 'AdminDMCompany', params: {id: props.item.company.id} }">
-                  {{ props.item.company.name }}
-                </router-link>
-              </td>
-              <td>
-                <router-link :to="{ name: 'AdminDMTalent', params: {id: props.item.talent.id} }">
-                  {{ props.item.talent.firstName }} {{ props.item.talent.lastName.toUpperCase() }}
-                </router-link>
-              </td>
-              <td>
+      <v-layout wrap>
+        <v-flex xs6 class="pb-3 pr-3">
+          <v-card>
+            <requirement-select
+              v-model="requirement"
+              @input="searchInput = ''; getInterviews()"
+              class="pa-3"
+            ></requirement-select>
+          </v-card>
+        </v-flex>
+        <v-flex xs6 class="pb-3">
+          <v-card>
+            <search-box
+              v-model="searchInput"
+              @search="requirement = null; getInterviews()"
+              class="pa-3"
+            ></search-box>
+          </v-card>
+        </v-flex>
+        <v-flex xs12>
+          <v-card>
+            <v-data-table
+              :headers="headers"
+              :items="interviews"
+              :loading="loading"
+              :pagination.sync="pagination"
+              :total-items="totalItems"
+            >
+              <template slot="items" slot-scope="props">
+                <td>{{ props.item.createdAt | formatDate('LLL') }}</td>
+                <td>
+                  <router-link :to="{ name: 'AdminDMCompany', params: {id: props.item.company.id} }">
+                    {{ props.item.company.name }}
+                  </router-link>
+                </td>
+                <td>
+                  <router-link :to="{ name: 'AdminDMTalent', params: {id: props.item.talent.id} }">
+                    {{ props.item.talent.firstName }} {{ props.item.talent.lastName.toUpperCase() }}
+                  </router-link>
+                </td>
+                <td>
               <span :class="[`${getInterviewStatusColor(props.item.status)}--text`, 'font-weight-bold']">
                 {{ props.item.status }}
               </span>
-              </td>
-              <td>{{ props.item.startAt | formatDate('LLL') }}</td>
-              <td class="justify-center layout">
-                <v-btn icon color="primary" :to="{ name: 'AdminDMInterview', params: {id: props.item.id} }">
-                  <v-icon>visibility</v-icon>
-                </v-btn>
-              </td>
-            </template>
-          </v-data-table>
-        </v-card>
-      </v-flex>
+                </td>
+                <td>{{ props.item.startAt | formatDate('LLL') }}</td>
+                <td class="justify-center layout">
+                  <v-btn icon color="primary" :to="{ name: 'AdminDMInterview', params: {id: props.item.id} }">
+                    <v-icon>visibility</v-icon>
+                  </v-btn>
+                </td>
+              </template>
+            </v-data-table>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-flex>
   </v-layout>
 </template>
@@ -57,12 +68,14 @@
   import { mapGetters, mapActions, mapState } from 'vuex';
   import DataManagementNavigation from '../Navigation';
   import RequirementSelect from '../../Utilities/RequirementSelect';
+  import SearchBox from '../../Utilities/SearchBox';
 
   export default {
     name: 'data-management-interviews',
     components: {
       DataManagementNavigation,
       RequirementSelect,
+      SearchBox,
     },
     data: () => ({
       interviews: [],
@@ -80,6 +93,7 @@
         descending: true,
       },
       requirement: null,
+      searchInput: '',
     }),
     computed: {
       ...mapGetters([
@@ -106,14 +120,17 @@
       ]),
       getInterviews() {
         this.prepareForApiConsumption();
-        const path = '/interviews';
+        let path = '/interviews';
         let queryString = 'projection=admin-item';
         if (this.requirement) {
           queryString += `&opportunity.requirement=${this.requirement.id}`;
+        } else if (this.searchInput) {
+          path += '/search/findByTalentLastNameContainingOrTalentFirstNameContainingAllIgnoreCase';
+          queryString += `&talentLastName=${this.searchInput}&talentFirstName=${this.searchInput}`;
         }
         queryString += `&page=${this.pagination.page - 1}&size=${this.pagination.rowsPerPage}`;
         queryString += this.pagination.sortBy ? `&sort=${this.pagination.sortBy},${this.pagination.descending ? 'desc' : 'asc'}` : '';
-        this
+        return this
           .api(`${path}?${queryString}`)
           .then((response) => {
             this.interviews = response.data._embedded.interviews;
