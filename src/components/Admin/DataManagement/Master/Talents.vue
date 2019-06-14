3ec26fc10042ad6d028edf319ac07331b704c339
Martin Jeannot <m4rtinjeannot@gmail.com>
2019-06-15 01:23:10 +0200

    [Admin/Talents] improve search UX

--- src/components/Admin/DataManagement/Master/Talents.vue
+++ src/components/Admin/DataManagement/Master/Talents.vue
@@ -4,41 +4,54 @@
       <data-management-navigation></data-management-navigation>
     </v-flex>
     <v-flex xs10>
-      <v-card>
-        <v-card-title>
-          <v-spacer></v-spacer>
-          <v-text-field label="Search..." v-model="search" @blur="getTalents" append-icon="search" single-line
-                        hide-details></v-text-field>
-        </v-card-title>
-        <v-data-table :items="talents" :headers="headers" :pagination.sync="pagination" :total-items="totalItems"
-                      :loading="loading">
-          <template slot="items" slot-scope="props">
-            <td>
-              <v-avatar size="46">
-                <v-img
-                  :src="props.item.profilePictureUrl"
-                  lazy-src="static/img/avatar.png"
-                  alt="picture"
-                ></v-img>
-              </v-avatar>
-            </td>
-            <td>{{ props.item.lastName }}</td>
-            <td>{{ props.item.firstName }}</td>
-            <td>{{ props.item.email }}</td>
-            <td>{{ props.item.createdAt | formatDate('LLL') }}</td>
-            <td>
-              <v-icon :color="props.item.active ? 'green' : 'red'">
-                {{ props.item.active ? 'done' : 'clear' }}
-              </v-icon>
-            </td>
-            <td class="justify-center layout">
-              <v-btn icon color="primary" :to="{ name: 'AdminDMTalent', params: {id: props.item.id} }">
-                <v-icon>visibility</v-icon>
-              </v-btn>
-            </td>
-          </template>
-        </v-data-table>
-      </v-card>
+      <v-layout wrap>
+        <v-flex xs6 offset-xs3 class="pb-3">
+          <v-card>
+            <search-box
+              v-model="searchInput"
+              @search="getTalents"
+              class="pa-3"
+            ></search-box>
+          </v-card>
+        </v-flex>
+        <v-flex xs12>
+          <v-card>
+            <v-data-table
+              :headers="headers"
+              :items="talents"
+              :loading="loading"
+              :pagination.sync="pagination"
+              :total-items="totalItems"
+            >
+              <template slot="items" slot-scope="props">
+                <td>
+                  <v-avatar size="46">
+                    <v-img
+                      :src="props.item.profilePictureUrl"
+                      lazy-src="static/img/avatar.png"
+                      alt="picture"
+                    ></v-img>
+                  </v-avatar>
+                </td>
+                <td>{{ props.item.lastName }}</td>
+                <td>{{ props.item.firstName }}</td>
+                <td>{{ props.item.email }}</td>
+                <td>{{ props.item.createdAt | formatDate('LLL') }}</td>
+                <td>
+                  <v-icon :color="props.item.active ? 'green' : 'red'">
+                    {{ props.item.active ? 'done' : 'clear' }}
+                  </v-icon>
+                </td>
+                <td class="justify-center layout">
+                  <v-btn icon color="primary" :to="{ name: 'AdminDMTalent', params: {id: props.item.id} }">
+                    <v-icon>visibility</v-icon>
+                  </v-btn>
+                </td>
+              </template>
+            </v-data-table>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-flex>
   </v-layout>
 </template>
@@ -46,10 +59,14 @@
 <script>
   import { mapGetters, mapActions } from 'vuex';
   import DataManagementNavigation from '../Navigation';
+  import SearchBox from '../../Utilities/SearchBox';
 
   export default {
     name: 'data-management-talents',
-    components: { DataManagementNavigation },
+    components: {
+      DataManagementNavigation,
+      SearchBox,
+    },
     data: () => ({
       talents: [],
       headers: [
@@ -66,13 +83,16 @@
         sortBy: 'createdAt',
         descending: true,
       },
-      search: '',
+      searchInput: '',
     }),
     computed: {
       ...mapGetters([
         'api',
         'loading',
       ]),
+      encodedSearchInput() {
+        return encodeURIComponent(this.searchInput);
+      },
     },
     watch: {
       pagination: {
@@ -91,11 +111,11 @@
       getTalents() {
         this.prepareForApiConsumption();
         let path = '/talents';
-        path += this.search ? '/search/findByEmailContainingOrLastNameContainingOrFirstNameContainingOrPhoneNumberContainingAllIgnoreCase' : '';
+        path += this.searchInput ? '/search/findByEmailContainingOrLastNameContainingOrFirstNameContainingOrPhoneNumberContainingAllIgnoreCase' : '';
         let queryString = '';
         queryString += `&page=${this.pagination.page - 1}&size=${this.pagination.rowsPerPage}`;
         queryString += this.pagination.sortBy ? `&sort=${this.pagination.sortBy},${this.pagination.descending ? 'desc' : 'asc'}` : '';
-        queryString += this.search ? `&email=${this.search}&lastName=${this.search}&firstName=${this.search}&phoneNumber=${this.search}` : '';
+        queryString += this.searchInput ? `&email=${this.encodedSearchInput}&lastName=${this.encodedSearchInput}&firstName=${this.encodedSearchInput}&phoneNumber=${this.encodedSearchInput}` : '';
         this
           .api(`${path}?${queryString}`)
           .then((response) => {
