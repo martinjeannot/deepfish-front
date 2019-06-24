d95907efca9549b6f7304dfd6a76d2db27a30f07
Martin Jeannot <m4rtinjeannot@gmail.com>
2019-06-24 11:10:02 +0200

    [Admin/Util] add opportunity select component

---
+++ src/components/Admin/Utilities/OpportunitySelect.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-autocomplete
+    v-model="opportunity"
+    clearable
+    :disabled="!talent"
+    hide-details
+    item-text="company.name"
+    :items="opportunities"
+    label="Opportunity"
+    :loading="loading"
+    prepend-icon="assignment"
+    return-object
+    :search-input.sync="searchInput"
+  ></v-autocomplete>
+</template>
+
+<script>
+  import { debounce } from 'lodash';
+  import { mapGetters, mapActions } from 'vuex';
+
+  export default {
+    name: 'OpportunitySelect',
+    props: [
+      'value',
+      'talent',
+    ],
+    data: () => ({
+      opportunities: [],
+      searchInput: '',
+      loading: false,
+    }),
+    computed: {
+      ...mapGetters([
+        'api',
+      ]),
+      opportunity: {
+        get() {
+          return this.value;
+        },
+        set(value) {
+          this.$emit('input', value);
+        },
+      },
+    },
+    watch: {
+      searchInput(value) {
+        if (value && value.length >= 3) {
+          return this.debouncedSearchOpportunities(value);
+        }
+        return null;
+      },
+    },
+    methods: {
+      ...mapActions([
+        'showSnackbar',
+      ]),
+      searchOpportunities(searchInput) {
+        this.loading = true;
+        this.api(`/opportunities/search/findInterviewSchedulableByTalentIdAndCompanyNameContainingAllIgnoreCase?projection=admin-item&talentId=${this.talent.id}&companyName=${encodeURIComponent(searchInput)}`)
+          .then((response) => {
+            this.opportunities = response.data._embedded.opportunities;
+          })
+          .catch(() => this.showSnackbar(['Error', 'error']))
+          .finally(() => {
+            this.loading = false;
+          });
+      },
+    },
+    created() {
+      this.debouncedSearchOpportunities = debounce(this.searchOpportunities, 600);
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
