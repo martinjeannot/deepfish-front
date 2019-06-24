9dc9cb613db8e0844260b616cfed8f78a29d8a30
Martin Jeannot <m4rtinjeannot@gmail.com>
2019-06-24 11:41:28 +0200

    [Admin/Util] add participation status select component

---
+++ src/components/Admin/Utilities/ParticipationStatusSelect.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-select
+    v-model="participationStatus"
+    :items="participationStatuses"
+    :label="label"
+    :readonly="readonly"
+  >
+    <template #selection="{ item }">
+      <div :class="[`${item.color}--text`]">
+        {{ item.text }}
+      </div>
+    </template>
+    <template #item="{ item }">
+      <div :class="[`${item.color}--text`]">
+        {{ item.text }}
+      </div>
+    </template>
+  </v-select>
+</template>
+
+<script>
+  export default {
+    name: 'ParticipationStatusSelect',
+    props: [
+      'value',
+      'label',
+      'readonly',
+    ],
+    data: () => ({
+      participationStatuses: [
+        {
+          text: 'Needs action',
+          value: 'NEEDS_ACTION',
+          color: 'warning',
+        },
+        {
+          text: 'Accepted',
+          value: 'ACCEPTED',
+          color: 'success',
+        },
+      ],
+    }),
+    computed: {
+      participationStatus: {
+        get() {
+          return this.value;
+        },
+        set(value) {
+          this.$emit('input', value);
+        },
+      },
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
