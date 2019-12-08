6ef9e531407a90380c59eedb6f241562faddc3f3
Martin Jeannot <m4rtinjeannot@gmail.com>
2019-12-08 23:38:37 +0100

    [Employer/Search] make talent disappear upon opportunity sending

--- src/components/Employer/Search/TalentOverview.vue
+++ src/components/Employer/Search/TalentOverview.vue
@@ -1,59 +1,62 @@
 <template>
-  <v-card
-    @click="talentDialog = true"
-  >
-    <employer-talent
-      v-if="talentDialog"
-      :value.sync="talentDialog"
-      :id="talent.id"
-      @opportunity-sent="onOpportunitySent"
-    ></employer-talent>
-    <v-card-text>
-      <v-layout wrap>
-        <v-flex xs4>
-          <v-avatar size="50">
-            <v-img
-              :src="profilePictureUrl"
-              alt="picture"
-            ></v-img>
-          </v-avatar>
-        </v-flex>
-        <v-flex xs8>
-          <v-layout wrap>
-            <v-flex xs12>
-              <span class="font-weight-bold">{{ talent.firstName }}</span>
-            </v-flex>
-            <v-flex xs12>
-              BizDev dans une super ESN
-            </v-flex>
-          </v-layout>
-        </v-flex>
-        <v-flex xs12>
-          <v-icon>school</v-icon>
-          Très fort à l'école
-        </v-flex>
-        <v-flex xs12>
-          <v-divider></v-divider>
-        </v-flex>
-        <v-flex xs6>
-          <div class="text-xs-center subheading grey--text text--darken-2">
-            Salaire fixe
-          </div>
-          <div class="text-xs-center title">
-            {{ baseSalary }} k€
-          </div>
-        </v-flex>
-        <v-flex xs6>
-          <div class="text-xs-center subheading grey--text text--darken-2">
-            Expérience
-          </div>
-          <div class="text-xs-center title">
-            {{ talent.yearsOfExperience }} ans
-          </div>
-        </v-flex>
-      </v-layout>
-    </v-card-text>
-  </v-card>
+  <v-fade-transition>
+    <v-card
+      v-show="!hideTalentOverview"
+      @click="talentDialog = true"
+    >
+      <employer-talent
+        v-if="talentDialog"
+        :value.sync="talentDialog"
+        :id="talent.id"
+        @opportunity-sent="onOpportunitySent"
+      ></employer-talent>
+      <v-card-text>
+        <v-layout wrap>
+          <v-flex xs4>
+            <v-avatar size="50">
+              <v-img
+                :src="profilePictureUrl"
+                alt="picture"
+              ></v-img>
+            </v-avatar>
+          </v-flex>
+          <v-flex xs8>
+            <v-layout wrap>
+              <v-flex xs12>
+                <span class="font-weight-bold">{{ talent.firstName }}</span>
+              </v-flex>
+              <v-flex xs12>
+                BizDev dans une super ESN
+              </v-flex>
+            </v-layout>
+          </v-flex>
+          <v-flex xs12>
+            <v-icon>school</v-icon>
+            Très fort à l'école
+          </v-flex>
+          <v-flex xs12>
+            <v-divider></v-divider>
+          </v-flex>
+          <v-flex xs6>
+            <div class="text-xs-center subheading grey--text text--darken-2">
+              Salaire fixe
+            </div>
+            <div class="text-xs-center title">
+              {{ baseSalary }} k€
+            </div>
+          </v-flex>
+          <v-flex xs6>
+            <div class="text-xs-center subheading grey--text text--darken-2">
+              Expérience
+            </div>
+            <div class="text-xs-center title">
+              {{ talent.yearsOfExperience }} ans
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-card-text>
+    </v-card>
+  </v-fade-transition>
 </template>
 
 <script>
@@ -69,6 +72,7 @@
       talent: Object,
     },
     data: () => ({
+      hideTalentOverview: false,
       talentDialog: false,
     }),
     computed: {
@@ -88,6 +92,7 @@
       ]),
       onOpportunitySent() {
         this.showSnackbar(['Offre envoyée', 'success']);
+        this.hideTalentOverview = true;
       },
     },
   };
